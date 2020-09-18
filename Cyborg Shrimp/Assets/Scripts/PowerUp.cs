5adc0ab9c3081dc462ac1dbc19150c738c15fe31
Anthony Romrell <anthonyromrell@mac.com>
2020-09-17 22:59:29 -0600

    events

--- Cyborg Shrimp/Assets/Scripts/PowerUp.cs
+++ Cyborg Shrimp/Assets/Scripts/PowerUp.cs
@@ -1,13 +1,15 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.Events;
 
 public class PowerUp : MonoBehaviour
 {
     public int value;
-
+    public UnityEvent powerUpEvent;
+    
     private void OnTriggerEnter(Collider other)
     {
         value++;
         print(value);
+        powerUpEvent.Invoke();
     }
 }
