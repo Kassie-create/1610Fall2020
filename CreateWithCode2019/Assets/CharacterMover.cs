17b3d358bfac0a6c968b7786a346bc0450299e90
Anthony Romrell <anthonyromrell@mac.com>
2020-09-22 15:54:44 -0600

    Added Create with code and character controller

---
+++ CreateWithCode2019/Assets/CharacterMover.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMover : MonoBehaviour
+{
+
+    public CharacterController controller;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        controller.Move(Vector3.up);
+    }
+}
