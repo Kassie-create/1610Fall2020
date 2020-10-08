beef878a9bbbac2da0c85f501a5b9f2258733f46
Anthony Romrell <anthonyromrell@mac.com>
2020-10-08 12:21:14 -0600

    bool data

---
+++ Cyborg Shrimp/Assets/Scripts/MoveLeft.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class MoveLeft : MonoBehaviour
+{
+    public float speed = 3f;
+    public BoolData gameOver;
+
+    private void Update()
+    {
+        if (gameOver.value == false)
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+    }
+}
---
+++ Cyborg Shrimp/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody PlayerRb;
+    public float jumpForce;
+    public float gravityModifier;
+    public bool isOnGround;
+    public BoolData gameOver;
+    
+    
+    void Start()
+    {
+        PlayerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
+        gameOver.value = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)&& isOnGround)
+        {
+            PlayerRb.AddForce(Vector3.up * 8, ForceMode.Impulse);
+            isOnGround = false;
+        }
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }else if (collision.other.CompareTag("Obstacle"))
+        {
+            gameOver.value = true;
+            Debug.Log("Game Over");
+        }
+    }
+}
