5f4b074640f79f768aaffbc6452a2c73097289d2
Anthony Romrell <anthonyromrell@mac.com>
2020-10-29 22:53:58 -0600

    SO Review

--- Cyborg Shrimp/Assets/ScriptableObjects/Collectible.cs
+++ Cyborg Shrimp/Assets/ScriptableObjects/Collectible.cs
@@ -3,7 +3,6 @@
 [CreateAssetMenu]
 public class Collectible : ScriptableObject
 {
-    public float powerLevel = 1f;
     public int cashValue = 10;
     public Sprite art;
     public Color artColor = Color.blue;
