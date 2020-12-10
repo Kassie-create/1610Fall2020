8b423d5b108041ed71d3b83b5a79622d3fbe31ed
Anthony Romrell <anthonyromrell@mac.com>
2020-12-10 14:24:10 -0700

    Collections

---
+++ Cyborg Shrimp/Assets/CoinCollection.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class CoinCollection : ScriptableObject
+{
+    public List<Transform> collectedTransforms;
+
+    public void Collect(Transform obj)
+    {
+        collectedTransforms.Add(obj);
+    }
+
+    public void PrintCollection()
+    {
+        foreach (var obj in collectedTransforms)
+        {
+            Debug.Log(obj);   
+        }
+    }
+}
